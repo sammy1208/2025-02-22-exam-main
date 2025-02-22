d85e0ed144556d08efa9866cdb32e8aba7ed3100
sammy1208 <34804688+sammy1208@users.noreply.github.com>
2025-02-22 16:09:28 +0800

    上傳

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ function App() {
           value={selectValue}
           onChange={(e) => inputSearch(e)}
           >
-          {selectData.map((item, index) => (
+          {categories.map((item, index) => (
             <option
 
             value={item}
@@ -136,7 +136,7 @@ function App() {
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                   </svg>
                   <span className="ml-1 text-gray-600">
-                    {`${data.rating.rate} (${data.rating.count})`}
+                    {/* {`${data.rating.rate} (${data.rating.count})`} */}
                   </span>
                 </div>
                 <span className="bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-sm">
